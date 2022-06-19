ac5b4edce687ad57ba62f7812b20bf7b56180db0
Ali Zeinodin <alizeinodin79@gmail.com>
2022-06-19 17:21:33 +0430

    singleton pattern delete from Player class

--- Player.php
+++ Player.php
@@ -2,20 +2,7 @@
 
 class Player // singleton for player class
 {
-    private static $obg; // object of this class
-
     protected $players = [];
-
-    /**
-     * singleton for this class
-     *
-     * @return Player
-     */
-    public static function singleton(): Player
-    {
-        isset(self::$obg) ?: self::$obg = new player();
-        return self::$obg;
-    }
 
     /**
      * add players to player array
