c20bdf641f758300d1400eeb2371e2ff60c0a769
Ali Zeinodin <alizeinodin79@gmail.com>
2022-07-14 16:03:04 +0430

    feature's of rules.html added to rules.php

--- front/html/rules.php
+++ front/html/rules.php
@@ -13,22 +13,40 @@
     <div class="form-group">
         <input type="text" class="form-control" aria-describedby="testHelp1"
                placeholder="نقش شهروندان را وارد کنید"
-               name="frm[citizen]"
-        >
+               name="frm[citizen]">
         <div id="textHelp1" class="form-text text-light">نقش ها را با کاما (,) از یکدیگر جدا کنید</div>
+        <div class="input-group mb-3">
+            <input type="range" class="form-range " id="rangeCitizen" min="1" max="6" dir="ltr"
+                   oninput="updateLabel('numberCitizen', 'rangeCitizen')"
+                   value="3">
+            <div class="text-light">
+                <p class="text-right">تعداد شهروندان: <span class="text-info" id="numberCitizen">3</span></p>
+            </div>
+        </div>
     </div>
     <div class="form-group mt-3">
         <input type="text" class="form-control" aria-describedby="testHelp2"
                placeholder="نقش مافیا ها را وارد کنید"
-               name="frm[mafia]"
-        >
+               name="frm[mafia]">
         <div id="textHelp2" class="form-text text-light">اسامی را با کاما (,) از یکدیگر جدا کنید</div>
+        <div class="input-group mb-3">
+            <input type="range" class="form-range" id="rangeMafia" min="1" max="6" dir="ltr"
+                   oninput="updateLabel('numberMafia', 'rangeMafia')"
+                   value="3">
+            <div class="text-light">
+                <p class="text-right">تعداد مافیا ها: <span class="text-info" id="numberMafia">3</span></p>
+            </div>
+        </div>
     </div>
     <div class="form-group">
         <button type="submit" class="btn btn-outline-light btn-block w-100 mt-4" name="frm[btn]">ثبت</button>
     </div>
 </form>
-
+<script>
+    function updateLabel(label, range) {
+        document.getElementById(label).innerHTML = document.getElementById(range).value;
+    }
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
         crossorigin="anonymous"></script>
