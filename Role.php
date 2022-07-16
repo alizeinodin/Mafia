4f4e86f3920e1cac33999d43d25938198b635d25
Ali Zeinodin <alizeinodin79@gmail.com>
2022-07-16 14:36:05 +0430

    validation and roleMaker method's added in Role.php

--- Role.php
+++ Role.php
@@ -45,5 +45,37 @@ class Role
         return count($this->roles);
     }
 
+    /**
+     * convert all role's of mafia and citizen to an array
+     * for random selection role for players by program
+     *
+     * @param array $citizen
+     * @param int $citizenNumber
+     * @param array $mafia
+     * @param int $mafiaNumber
+     * @param int $playerNumber
+     */
+    public function roleMaker(array $citizen, int $citizenNumber, array $mafia, int $mafiaNumber, int $playerNumber)
+    {
+        $this->validation(count($citizen), $citizenNumber, count($mafia), $mafiaNumber, $playerNumber);
+    }
+
+    /**
+     * checking validation for equalization of number's of citizen and mafia role's and players
+     * citizen array and mafia array members can lower or equal than citizenNumber and mafiaNumber
+     * citizen and mafia number must equal to player numbers
+     *
+     * @param $citizenIs
+     * @param $citizenExpected
+     * @param $mafiaIs
+     * @param $mafiaExpected
+     * @param $playerNumber
+     */
+    private function validation($citizenIs, $citizenExpected, $mafiaIs, $mafiaExpected, $playerNumber)
+    {
+
+    }
+
+
 
 }
